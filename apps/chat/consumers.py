101c90f33b8fda720a2341248aa5f3176c71e1dc
hwandev12 <husan.ahmedov777@yandex.com>
2023-06-30 19:07:42 +0500

    show email in chat

--- apps/chat/consumers.py
+++ apps/chat/consumers.py
@@ -32,18 +32,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        email = text_data_json["email"]
 
         # await save_message(message, self.scope["user"])
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name, {
+                "type": "chat_message",
+                "message": message,
+                "email": email,
+            }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
-
+        email = event["email"]
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message, "email": email}))
  
 # @database_sync_to_async
 # def save_message(message, user):
