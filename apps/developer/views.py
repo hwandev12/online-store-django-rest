114e4db8d8bc21726660e6732dbda08e5441c84c
hwandev12 <husan.ahmedov777@yandex.com>
2023-07-07 18:47:49 +0500

    last ordered

--- apps/developer/views.py
+++ apps/developer/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from apps.product.models import Product
+from apps.product.models import Product, Checkout
 
 from django.contrib.auth.mixins import UserPassesTestMixin
+
+from datetime import datetime, timedelta
+
 class DeveloperHomePage(UserPassesTestMixin ,ListView):
     template_name = 'developer/pages/develop.html'
     context_object_name = 'apps'
@@ -14,6 +17,11 @@ class DeveloperHomePage(UserPassesTestMixin ,ListView):
     def get_queryset(self, *args, **kwargs):
         qs = super(DeveloperHomePage, self).get_queryset(*args, **kwargs)
         return qs
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(DeveloperHomePage, self).get_context_data(*args, **kwargs)
+        context['recent_sales'] = Checkout.objects.filter(ordered_date__gte=datetime.now()-timedelta(days=5)).order_by('-ordered_date')
+        return context
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
