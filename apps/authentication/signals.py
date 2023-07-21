e9aafc91e28a420203da02dbdb21136482ac1b20
hwandev12 <husan.ahmedov777@yandex.com>
2023-07-21 21:03:12 +0500

    fix save issues committed by save method

--- apps/authentication/signals.py
+++ apps/authentication/signals.py
@@ -13,10 +13,6 @@ from django.dispatch import receiver
 def create_buyer_profile(sender, instance, created, **kwargs):
     if created:
         BuyerProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=BuyerAccountModel)
-def save_buyer_profile(sender, instance, **kwargs):
-    instance.buyerprofile.save()
     
     
 # for seller profile 
@@ -24,8 +20,4 @@ def save_buyer_profile(sender, instance, **kwargs):
 def create_seller_profile(sender, instance, created, **kwargs):
     if created:
         SellerProfile.objects.create(user=instance)
-        
-@receiver(post_save, sender=SellerAccountModel)
-def save_seller_profile(sender, instance, **kwargs):
-    instance.sellerprofile.save()
     
